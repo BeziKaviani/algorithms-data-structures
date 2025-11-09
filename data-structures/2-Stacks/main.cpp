d3bf74e4855c8f8a8d28c8c332e609328c0327b2
unknown <behzadkaviani2004@gmail.com>
2025-11-10 00:56:07 +0330

    completed stacks chapter

--- data-structures/2-Stacks/main.cpp
+++ data-structures/2-Stacks/main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
 
+int main(){
+//    Stack is a LIFO data structure which means Last In First Out
+    stack<string> fruits;
+//    You can add an item in stack. We call it push
+    fruits.push("Apple");
+    fruits.push("Orange");
+    fruits.push("Banana");
+//    The last item is banana. So it is the top item
+    cout<<fruits.top()<<endl;
+
+//    You can remove an item in stack. It is called pop
+    fruits.pop();
+//    We have removed an item from the top by using pop function
+    cout<<fruits.top()<<endl;
+
+//    We can change the top item with top function
+    fruits.top() = "Cherry";
+    cout<<fruits.top()<<endl;
+
+//    You can find the size of the stack by using size function
+    cout<<fruits.size()<<endl;
+
+//    You can see if the stack is empty by using empty method
+    cout<<fruits.empty()<<endl;
+
+    return 0;
+}
