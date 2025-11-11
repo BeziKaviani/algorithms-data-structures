fae0b7b2d282baa6f2e17fe0831b3d00a3a3f89d
unknown <behzadkaviani2004@gmail.com>
2025-11-11 19:44:35 +0330

    completed queues chapter

--- data-structures/3-Queues/main.cpp
+++ data-structures/3-Queues/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include <string>
+#include <queue>
 
 using namespace std;
 
 int main(){
+//    Queue is a FIFO data structure. Which means the first in first out.
+    queue<string> orders;
 
+//    You can enter items in the queue by using push method
+    orders.push("Pizza");
+    orders.push("Burger");
+    orders.push("Hot dog");
+
+//    You can access to the first item b using front method
+    cout<<orders.front()<<endl;
+
+//  Also, you can access to the item in the back by using back method
+    cout<<orders.back()<<endl;
+
+//    You can initialize the items by using front and back method
+    orders.front() = "Kebab";
+    orders.back() = "Fried chicken";
+    cout<<orders.front()<<endl;
+    cout<<orders.back()<<endl;
+
+//    Items can be removed from the queue using pop method
+    orders.pop();
+    cout<<orders.front()<<endl;
+
+//    You can check if the queue is empty by using empty method
+    cout<<orders.empty()<<endl;
     return 0;
 }
